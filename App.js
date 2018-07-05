4c5dadd50e503a8d676fd9c4e9400767d85cc5d0
jeffwhenderson <jeffrey.henderson17@gmail.com>
2018-07-05 16:47:16 -0400

    [JH][#000] initial commit of react Native App.

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>8=====D</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(200, 10, 10)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
